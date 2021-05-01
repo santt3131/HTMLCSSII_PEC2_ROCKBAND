a4b3e2df1e0ca15709a686f3e5b4c26ac85088a7
santt3131 <santt31@gmail.com>
2021-05-01 22:49:06 +0200

    upload project

---
+++ src/assets/scripts/main.js
@@ -0,0 +1,12 @@
+/**
+ * Import dependencies from node_modules
+ * see commented examples below
+ */
+
+// import 'some-node-module';
+// import SomeModule from 'some-node-module';
+
+/**
+ * Write any meet JavaScript below
+ */
+import * as bootstrap from 'bootstrap';
